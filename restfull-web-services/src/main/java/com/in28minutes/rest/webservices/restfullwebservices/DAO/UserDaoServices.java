6f0d52bbd6a1380394c75da4c0bbeaa051b32195
HugoSanche <hbmclaren@gmail.com>
2024-01-20 19:28:22 -0600

    Add POST method save

--- restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfullwebservices/DAO/UserDaoServices.java
+++ restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfullwebservices/DAO/UserDaoServices.java
@@ -6,19 +6,43 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoServices {
 
+    private static Integer countId=0;
     private static List<User> users=new ArrayList<>();
     static {
-                users.add(new User(1,"Hugo", LocalDate.now().minusYears(30)));
-                users.add(new User(1,"Hugo", LocalDate.now().minusYears(30)));
-                users.add(new User(1,"Hugo", LocalDate.now().minusYears(30)));
+                users.add(new User(++countId,"Hugo", LocalDate.now().minusYears(30)));
+                users.add(new User(++countId,"Veronica", LocalDate.now().minusYears(40)));
+                users.add(new User(++countId,"Daniel", LocalDate.now().minusYears(50)));
     }
     public List<User> listAll(){
         return users;
     }
 
+    public User listUser(int id){
+        return users.get(id);
+    }
+    public User findOne(int id){
+        Predicate<? super User> predicate=user ->user.getId().equals(id);
+        System.out.println("Result Predicate "+predicate);
+        return users.stream().filter(predicate).findFirst().get();
+    }
 
+    public void save(User user){
+        user.setId(++countId);
+        users.add(user);
+    }
 }
+
+
+
+
+
+
+
+
+
+
--- restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfullwebservices/controller/UserResource.java
+++ restfull-web-services/src/main/java/com/in28minutes/rest/webservices/restfullwebservices/controller/UserResource.java
@@ -1,2 +1,33 @@
-package com.in28minutes.rest.webservices.restfullwebservices.controller;public class UserResource {
+package com.in28minutes.rest.webservices.restfullwebservices.controller;
+
+import com.in28minutes.rest.webservices.restfullwebservices.DAO.UserDaoServices;
+import com.in28minutes.rest.webservices.restfullwebservices.entities.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+    UserDaoServices services;
+
+    public UserResource(UserDaoServices services) {
+        this.services = services;
+    }
+
+    //
+    @GetMapping("/users")
+    public List<User> retrieveAll(){
+        return services.listAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return services.findOne(id);
+    }
+   //@POST /users
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user){
+        services.save(user);
+    }
 }
